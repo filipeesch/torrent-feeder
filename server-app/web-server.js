47ea48ddb6924a6216b1075b21f7454ab846e898
filipeesch <filipeesch@hotmail.com>
2018-04-28 20:41:03 +0100

    first release

---
+++ server-app/web-server.js
@@ -0,0 +1,51 @@
+
+var sqlite3 = require('sqlite3').verbose();
+const express = require('express');
+var Promise = require('promise');
+var moment = require('moment');
+
+var db = new sqlite3.Database('database3.db');
+
+db.run("CREATE TABLE IF NOT EXISTS ShowRssItem (EpisodeID INT, ShowID INT, ShowName VARCHAR(100), EpisodeName VARCHAR(100), Link TEXT, Date VARCHAR(30), RawTitle VARCHAR(255), Downloaded INT, PRIMARY KEY(EpisodeID, ShowID))");
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'pug');
+app.locals.moment = moment;
+
+app.get('/', (request, response) => {
+
+    db.all(
+        "SELECT DISTINCT ShowID, ShowName FROM ShowRssItem ORDER BY ShowName",
+        function (err, shows) {
+
+            db.all(
+                "SELECT EpisodeID, EpisodeName, ShowName, Date, Downloaded FROM ShowRssItem WHERE $showID IS NULL OR ShowID = $showID ORDER BY EpisodeID DESC",
+                { $showID: request.query["showId"] },
+                function (err, episodes) {
+
+                    response.render("index", { shows: shows, episodes: episodes })
+                });
+        });
+});
+
+app.get('/download', (request, response) => {
+
+    db.run("UPDATE ShowRssItem  SET Downloaded = $downloaded WHERE EpisodeID = $episodeID",
+        {
+            $episodeID: request.query.episodeId,
+            $downloaded: request.query.downloaded,
+        },
+        function () {
+            response.redirect("/");
+        });
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+});
